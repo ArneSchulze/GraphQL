666fc87c27e1bdc2dd4394002caa7ff59628ec01
Arne Artur Schulze <arne.artur.schulze@sap.com>
2018-05-03 15:02:36 +0200

    Finalize tutorial

--- tutorial-script-final.js
+++ tutorial-script-final.js
@@ -24,24 +24,72 @@ function toMongo(item) {
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = `
+type Author {
+	id: ID!,
+	name: String!,
+	email: String!
+}
+
+type Post {
+	id: ID!,
+	title: String!,
+	description: String,
+	authors: [Author]!
+}
+
+input AuthorInput {
+	name: String!
+}
+
+input PostInput {
+	title: String!,
+  description: String,
+  authors: [ID!]!
+}
+
 type Query {
-	hello: String!
+	author(name: String!): [Author!]
+  post(title: String!): [Post!]
+}
+
+type Mutation {
+	insertAuthor(author: AuthorInput!): Author
+	insertPost(post: PostInput!): Post
 }
 `;
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: async (parent, input, context) => {
-        return hello;
+    author: async (parent, { name }, context) => {
+      return getAuthorByName(name, context.mongo);
+    },
+    post: async (parent, { title }, context) => {
+      return getPostByTitle(title, context.mongo);
+    }
+  },
+  Mutation: {
+    insertAuthor: async (parent, { author }, context) => {
+      console.log('mongo', context)
+      return insertAuthor(author, context.mongo);
+    },
+    insertPost: async (parent, { post }, context) => {
+      return insertPost(post, context.mongo);
+    }
+  },
+  Post: {
+    authors: async (parent, input, context) => {
+      console.log('parent', parent)
+      return getAuthorsByIds(parent.authors, context.mongo)
     }
   }
 };
 
+//returns the authors with the specified name
 async function getAuthorByName(name, mongo) {
   return mongo.collection('authors').find({ name: name }).map(fromMongo).toArray();
 }
-
+//returns the authors with the given ids
 async function getAuthorsByIds(ids, mongo) {
   var mapped = ids.map(id => new ObjectID(id));
   return mongo.collection('authors').find({ _id: { $in: mapped } }).map(fromMongo).toArray();
@@ -53,12 +101,12 @@ async function getPostByTitle(title, mongo) {
 
 async function insertAuthor(author, mongo) {
   var result = await mongo.collection('authors').insertOne(author);
-  return fromMongo(await mongo.collection('authors').findOne({ _id: result.insertedId }))
+  return fromMongo(await mongo.collection('authors').findOne({}, { _id: result.insertedId }))
 }
 
 async function insertPost(post, mongo) {
   var result = await mongo.collection('posts').insertOne(post);
-  return fromMongo(await mongo.collection('posts').findOne({ _id: result.insertedId }))
+  return fromMongo(await mongo.collection('posts').findOne({}, { _id: result.insertedId }))
 }
 
 export const schema = makeExecutableSchema({
